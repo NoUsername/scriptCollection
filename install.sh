b522db27d8ae5dee98b16297f4d703678a2ac0c3
NoUsername <paul@paukl.at>
2013-06-30 14:58:32 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $BASEDIR
 
+echo installing .bashrc_ext
 if [ -f $HOME/.bashrc ]; then
-	mv $HOME/.bashrc $HOME/.bashrc.bak
+	cp $HOME/.bashrc $HOME/.bashrc.bak
 fi
 if [ -f $HOME/.bashrc_ext ]; then
 	mv $HOME/.bashrc_ext $HOME/.bashrc_ext.bak
@@ -18,11 +19,26 @@ if [ "$?" != "0" ]; then
 	printf "\n\nif [ -f ~/.bashrc_ext ]; then\n    . ~/.bashrc_ext\nfi\n\n" >> $HOME/.bashrc
 fi
 
+echo installing .tmux.conf
 if [ -f $HOME/.tmux.conf ]; then
 	mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
 fi
 cp conf/tmux.conf $HOME/.tmux.conf
 
+echo installing shellscripts to /scripts/
 mkdir -p $HOME/scripts
+
+# copy a script to ~/scripts/ making backups of existing files
+SCRIPTNAME="script.sh"
+copyScriptSafe() {
+	if [ ! -f $HOME/scripts/$SCRIPTNAME ]; then
+		mv $HOME/scripts/$SCRIPTNAME $HOME/scripts/${SCRIPTNAME}.bak
+	fi
+	cp bash/$SCRIPTNAME $HOME/scripts/
+}
+
+SCRIPTNAME="backupDb.sh"
+copyScriptSafe
+
 cp bash/backupDb.sh $HOME/scripts/
 
