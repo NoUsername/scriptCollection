c855020b8a9a2f2d8021ce963746a564fc945515
Paul Klingelhuber <paul.klingelhuber@gmail.com>
2014-04-22 22:52:27 +0200

    update setScreenBrightness.sh

--- bash/setScreenBrightness.sh
+++ bash/setScreenBrightness.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
 # set screen brightness (very low on lenovo thinkpad twist)
-$(echo "40" > /sys/class/backlight/intel_backlight/brightness) || true
 
-exit 0
+# default value 40 (very dark) can be changed via an optional argument
+B_VAL=40
+if [ "$#" -gt "0" ]; then
+ B_VAL="$1"
+fi
+
+bash -c "echo \"${B_VAL}\" > /sys/class/backlight/intel_backlight/brightness"
+
